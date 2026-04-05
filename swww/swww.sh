23d6594077ba85e44f2431b774b41ed93154b58e
jmnguye <nguyen.jean.michel@gmail.com>
2026-04-05 07:51:58 +0200

    swww is now replace by awww

--- swww/swww.sh
+++ swww/swww.sh
@@ -2,11 +2,11 @@
 
 img=$(ls ~/Images/wallpapers/ | sort -R | tail -1)
 
-pgrep swww-daemon 2>&1
+pgrep awww-daemon 2>&1
 
 if [ $? -ne 0 ] 
 then
-  swww-daemon
+  awww-daemon
 fi
 
-for screen in $(swww query | awk -F ":" '{print $2}'); do swww img -o "$screen" ~/Images/wallpapers/${img} --transition-type wipe --transition-angle 30 --transition-step 90; done
+for screen in $(awww query | awk -F ":" '{print $2}'); do awww img -o "$screen" ~/Images/wallpapers/${img} --transition-type wipe --transition-angle 30 --transition-step 90; done
